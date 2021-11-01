221e1cee7e059cc6dbca062a67e10141417f1f8f
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2021-11-01 20:58:02 +0900

    LGFX_Spriteの確保の仕方を変更 (　LovyanGFX 0.4.0以降での動作を確認済 )

--- src/M5ImageAvatarLite.cpp
+++ src/M5ImageAvatarLite.cpp
@@ -28,15 +28,6 @@ void ImageAvatarLite::init(LGFX *gfx, const char* filename, bool is_change,
     loadConfig(*_json_fs, filename);
     this->_gfx = gfx;
     this->_filename = filename;
-    _lcd_sp      = new LGFX_Sprite(_gfx);
-    _head_sp     = new LGFX_Sprite(&_lcd_sp);
-    _mouth_sp    = new LGFX_Sprite(&_lcd_sp);
-    _mouth_op_sp = new LGFX_Sprite(&_lcd_sp);
-    _mouth_cl_sp = new LGFX_Sprite(&_lcd_sp);
-    _eye_l_sp    = new LGFX_Sprite(&_lcd_sp);
-    _eye_r_sp    = new LGFX_Sprite(&_lcd_sp);
-    _eye_op_sp   = new LGFX_Sprite(&_lcd_sp);
-    _eye_cl_sp   = new LGFX_Sprite(&_lcd_sp);
     _expression = expression;
     _mv = _config.getMoveParameters(_expression);
     initSprites(is_change);
