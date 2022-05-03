8619806a4cfe8574f0bc0896f23e9dd7ae1b3bf7
Takao Akaki <mongonta@gmail.com>
2022-05-04 00:28:26 +0900

    fix default SD

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,8 @@ M5GFX &gfx( M5.Lcd ); // aliasing is better than spawning two instances of LGFX
 
 // JSONファイルとBMPファイルを置く場所を切り替え
 // 開発時はSPIFFS上に置いてUploadするとSDカードを抜き差しする手間が省けます。
-fs::FS json_fs = SPIFFS; //SD; // JSONファイルの収納場所(SPIFFS or SD)
-fs::FS bmp_fs  = SPIFFS; //SD; // BMPファイルの収納場所(SPIFFS or SD)
+fs::FS json_fs = SD; // JSONファイルの収納場所(SPIFFS or SD)
+fs::FS bmp_fs  = SD; // BMPファイルの収納場所(SPIFFS or SD)
 
 using namespace m5imageavatar;
 const char* avatar_json[] = { "/json/M5AvatarLite00.json",  // ファイル名は32バイトを超えると不具合が起きる場合あり。
